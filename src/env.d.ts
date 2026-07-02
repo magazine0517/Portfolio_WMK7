88c4543cf647f9d2f8ac4cea8088b2fead9d941d
Ching <magazine0517@gapp.nthu.edu.tw>
2026-07-02 16:10:39 +0800

    [Update]move Id from .env to code

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_SANITY_PROJECT_ID: string;
   readonly PUBLIC_SANITY_DATASET: string;
   readonly PUBLIC_SANITY_API_VERSION?: string;
   readonly SANITY_API_TOKEN?: string;
